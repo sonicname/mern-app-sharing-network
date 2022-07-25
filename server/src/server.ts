f0a6e091f72990f6d36724a58f067c56504aef60
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-25 10:13:23 +0700

    update base server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import expres, { Response, Request } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env["PORT"] || 3000;
+
+const app = expres();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+    message: "Hello world"
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}!`);
+});
