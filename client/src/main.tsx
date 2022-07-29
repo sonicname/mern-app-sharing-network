2610488c96a8bc691dcedceb44da52c80d31bbd3
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-29 07:15:47 +0700

    change to react.stricMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,30 +7,36 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import Loading from "./components/Loading";
 import { GlobalProvider } from "./contexts/global";
+import { AuthProvider } from "./contexts/auth";
+import React from "react";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const SignUpPage = lazy(() => import("./pages/SignUpPage"));
 const SignInPage = lazy(() => import("./pages/SignInPage"));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <GlobalProvider>
-      <Suspense
-        fallback={
-          <div className="fixed inset-0 bg-black bg-opacity-70">
-            <Loading padding={3} />
-          </div>
-        }
-      >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/signin" element={<SignInPage />} />
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-      <ToastContainer />
-    </GlobalProvider>
-  </>
+  <React.StrictMode>
+    <>
+      <GlobalProvider>
+        <AuthProvider>
+          <Suspense
+            fallback={
+              <div className="fixed inset-0 bg-black bg-opacity-70">
+                <Loading padding={3} />
+              </div>
+            }
+          >
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/signin" element={<SignInPage />} />
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
+          <ToastContainer />
+        </AuthProvider>
+      </GlobalProvider>
+    </>
+  </React.StrictMode>
 );
